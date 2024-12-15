b5a1559168e34028f0ecb95ae8bab4d67f3f76ed
andre-alck <andrealck1@gmail.com>
2024-12-15 19:33:59 -0300

    feat: add redirect logic to handler; add the same dependencies and tasks as main lambda

---
+++ app/src/main/java/com/asacxyz/App.java
@@ -0,0 +1,50 @@
+package com.asacxyz;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+public class App implements RequestHandler<Map<String, String>, Map<String, Object>> {
+  private static final String PATH_PARAMETER_KEY = "rawPath";
+  private static final String ERROR_VALUE = "error";
+  private static final String DEFAULT_URL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  private static final String BUCKET_NAME = "urlshortener-s3-asacxyz";
+
+  @Override
+  public Map<String, Object> handleRequest(Map<String, String> input, Context context) {
+    String shortenedUrlCode = input.get(App.PATH_PARAMETER_KEY).replace("/", "");
+    if (shortenedUrlCode == App.ERROR_VALUE) return this.buildRedirectResponse(App.DEFAULT_URL);
+
+    S3Client s3 = S3Client.builder().build();
+    try (InputStream originalLinkObjectStream = s3.getObject(GetObjectRequest.builder().bucket(App.BUCKET_NAME).key(shortenedUrlCode + ".json").build())) {
+      UrlData urlData = new ObjectMapper().readValue(originalLinkObjectStream, UrlData.class);
+
+      if (System.currentTimeMillis() < urlData.getExpirationTime()) return this.buildRedirectResponse(urlData.getOriginalUrl());
+    } catch (Exception e) {
+      this.log(e);
+    }
+
+    return this.buildRedirectResponse(App.DEFAULT_URL);
+  }
+
+  private Map<String, Object> buildRedirectResponse(String url) {
+    Map<String, Object> response = new HashMap<>();
+    response.put("statusCode", 302);
+
+    Map<String, String> headers = new HashMap<>();
+    headers.put("Location", url);
+    response.put("headers", headers);
+    return response;
+  }
+
+  private void log(Exception e) {
+    e.printStackTrace();
+  }
+}
---
+++ app/src/main/java/com/asacxyz/UrlData.java
@@ -0,0 +1,31 @@
+package com.asacxyz;
+
+import java.io.Serializable;
+
+public class UrlData implements Serializable {
+    private static final long serialVersionUID = 1;
+    
+    private String originalUrl;
+    private long expirationTime;
+
+    public UrlData(String originalUrl, long expirationTime) {
+        this.originalUrl = originalUrl;
+        this.expirationTime = expirationTime;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public long getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(long expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+}
